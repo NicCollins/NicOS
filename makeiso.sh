de5e1c7aca7f4c129ab1c8865663f848c5919752
Nicolas Collins <collins.nicolas@gmail.com>
2015-06-13 14:38:43 -0400

    added script to build iso

---
+++ makeiso.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Script to compile the Source code
+#and create an ISO image
+
+i686-elf-as boot.s -o boot.o
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o nicos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+mkdir -p isodir
+mkdir -p isodir/boot
+cp nicos.bin isodir/boot/
+mkdir -p isodir/boot/grub
+cp grub.cfg isodir/boot/grub/
+grub-mkrescue --output=nicos.iso isodir 
