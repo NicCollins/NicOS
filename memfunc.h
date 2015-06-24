9e6b861d61c871a63e7b8e488512ac457f1e09ad
Nicolas Collins <collins.nicolas@gmail.com>
2015-06-23 22:04:05 -0400

    added memory access functions

---
+++ memfunc.h
@@ -0,0 +1,9 @@
+#ifndef MEMFUNC_H
+#define MEMFUNC_H
+
+int memcmp(const void*, const void*, size_t);
+void* memcpy(void* __restrict, const void* __restrict, size_t);
+void* memmove(void*, const void*, size_t);
+void* memset(void*, int, size_t);
+
+#endif
